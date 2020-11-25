853d1f29a242ae72b61c9f839f6edd0dd9537e7d
Max Seo <seoj@google.com>
2020-11-25 06:32:49 -0800

    Remove unused

--- pwa-playground/service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', () => {
-  console.log('[ServiceWorker] Install');
-});
-
-self.addEventListener('fetch', (evt) => {
-  console.log('[ServiceWorker] Fetch', evt.request.url);
-});
